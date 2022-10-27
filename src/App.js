839b677af073b12a885fa409c1d3ab2cdd08924a
Michael Bishop <mrbishop5544@gmail.com>
2022-10-27 19:56:45 -0400

    initial commit and structuring with components

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import AboutMe from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Portfolio from "./components/Portfolio";
+import Resume from "./components/Resume";
+
+
+export default function App() {
+  return (
+    <main className="text-gray-400 bg-gray-900 body-font">
+      <Navbar />
+      <About />
+      <Portfolio />
+      <Resume />
+      <Contact />
+    </main>
+  );
+}
